046986cd3d6bbd37b1fe7055d44561999bd4cd50
Miquel Miró <miquel.miro@uib.cat>
2023-03-15 15:20:25 +0000

    The new USOLO now is working

--- urpn_torch/usolo/loss.py
+++ urpn_torch/usolo/loss.py
@@ -9,7 +9,7 @@ class DiceLoss(nn.Module):
         self.smooth = 0.1
 
     def forward(self, y_pred, y_true):
-        assert y_pred.size() == y_true.size()
+        assert y_pred.size() == y_true.size(), f"Sizes of prediction ({y_pred.size()}) and GT ({y_true.size()}) not equal"
         y_pred = y_pred[:, 0].contiguous().view(-1)
         y_true = y_true[:, 0].contiguous().view(-1)
 
@@ -42,8 +42,22 @@ class SoloLoss(nn.Module):
         self.__lambda = lambd
 
     def forward(self, mask_pred, cls_pred, mask_true, cls_true):
-        class_loss = torch.mean(sigmoid_focal_loss(cls_pred, cls_true))
-        mask_loss = self.__lambda * self.__loss_mask(mask_pred, cls_pred, mask_true)
-        loss = class_loss + mask_loss
+        loss = None
+        real_size_m = mask_true.shape[1]
+        real_size_c = cls_true.shape[0]
+        
+        for i in range(0, 5):
+            m_pred_part = mask_pred[:, i * real_size_m:(i+1) * real_size_m, :, :]
+            c_pred_part = cls_pred[i * real_size_c:(i+1) * real_size_c, :]
+            
+            class_loss = torch.mean(sigmoid_focal_loss(c_pred_part, cls_true))
+            mask_loss = self.__lambda * self.__loss_mask(m_pred_part, c_pred_part, mask_true)
+            
+            if loss is None:
+                loss = class_loss + mask_loss
+            else:
+                loss += (class_loss + mask_loss)
+        
+        loss /= 5
 
         return loss
--- urpn_torch/usolo/model.py
+++ urpn_torch/usolo/model.py
@@ -17,18 +17,31 @@ class URPN(model.UNet):
         cells: Tuple[int, int] = (5, 5),
         mask_size: Tuple[int, int] = (512, 512),
     ):
-        super(URPN, self).__init__(in_channels, features)
+        super(URPN, self).__init__(in_channels, features=features)
         self.__mask_size = mask_size
 
         # Class branch
+        self.cls_red5 = nn.Conv2d(in_channels=features * 16, out_channels=256, kernel_size=3)
+        self.cls_red4 = nn.Conv2d(in_channels=features * 8, out_channels=256, kernel_size=3)
+        self.cls_red3 = nn.Conv2d(in_channels=features * 4, out_channels=256, kernel_size=3)
+        self.cls_red2 = nn.Conv2d(in_channels=features * 2, out_channels=256, kernel_size=3)
+        self.cls_red1 = nn.Conv2d(in_channels=features, out_channels=256, kernel_size=3)
+        
         self.cls_branch = URPN._branch(size=256, name="cls", depth=7)
 
+        self.cls_out_1 = nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1)
         self.cls_out_1 = nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1)
         self.cls_out_2 = nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1)
         self.cls_out_3 = nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1)
         self.cls_out_4 = nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1)
         self.cls_out_5 = nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1)
 
+        self.mask_red5 = nn.Conv2d(in_channels=features * 16, out_channels=256, kernel_size=1)
+        self.mask_red4 = nn.Conv2d(in_channels=features * 8, out_channels=256, kernel_size=1)
+        self.mask_red3 = nn.Conv2d(in_channels=features * 4, out_channels=256, kernel_size=1)
+        self.mask_red2 = nn.Conv2d(in_channels=features * 2, out_channels=256, kernel_size=1)
+        self.mask_red1 = nn.Conv2d(in_channels=features, out_channels=256, kernel_size=3)
+        
         self.mask_branch = URPN._branch(size=256, name="branch", depth=7)
         self.mask_out_1 = nn.Conv2d(
             in_channels=256, out_channels=(cells[0] * cells[1]), kernel_size=1
@@ -58,12 +71,13 @@ class URPN(model.UNet):
         Returns:
             Pytorch Sequential of UNet blocks with depth and size defined by the parameters
         """
-        layers = [
-            (f"{name}_{level}", super()._block(size, size, name=f"{name}_{level}"))
+        layers = {
+            f"{name}_{level}": model.UNet.block(size, size, name=f"{name}_{level}")
             for level in range(1, depth)
-        ]
-
-        return nn.Sequential(OrderedDict([layers]))
+        }
+        layers = OrderedDict(layers)
+        
+        return nn.Sequential(layers)
 
     @staticmethod
     def _block_coord_conv(in_channels, features, name):
@@ -106,45 +120,55 @@ class URPN(model.UNet):
         bottleneck = self.bottleneck(self.pool4(enc4))
 
         # MASKS
-        al_mask5 = functional.interpolate(bottleneck, self.__mask_size)
+        red_mask5 = self.mask_red5(bottleneck)
+        al_mask5 = functional.interpolate(red_mask5, self.__mask_size)        
         mask5 = self.mask_branch(al_mask5)
         mask5 = self.mask_out_5(mask5)
 
-        al_mask4 = functional.interpolate(enc4, self.__mask_size)
+        red_mask4 = self.mask_red4(enc4)
+        al_mask4 = functional.interpolate(red_mask4, self.__mask_size)
         mask4 = self.mask_branch(al_mask4)
         mask4 = self.mask_out_4(mask4)
 
-        al_mask3 = functional.interpolate(enc3, self.__mask_size)
+        red_mask3 = self.mask_red3(enc3)
+        al_mask3 = functional.interpolate(red_mask3, self.__mask_size)
         mask3 = self.mask_branch(al_mask3)
         mask3 = self.mask_out_1(mask3)
-
-        al_mask2 = functional.interpolate(enc2, self.__mask_size)
+        
+        red_mask2 = self.mask_red2(enc2)
+        al_mask2 = functional.interpolate(red_mask2, self.__mask_size)
         mask2 = self.mask_branch(al_mask2)
         mask2 = self.mask_out_1(mask2)
 
-        al_mask1 = functional.interpolate(enc1, self.__mask_size)
+        red_mask1 = self.mask_red1(enc1)
+        al_mask1 = functional.interpolate(red_mask1, self.__mask_size)
         mask1 = self.mask_branch(al_mask1)
         mask1 = self.mask_out_1(mask1)
 
         # Class
-
-        al_cls5 = functional.interpolate(bottleneck, (5, 5))
+        
+        red_cls5 = self.cls_red5(bottleneck)
+        al_cls5 = functional.interpolate(red_cls5, (5, 5))
         cls5 = self.cls_branch(al_cls5)
         cls5 = self.cls_out_5(cls5)
 
-        al_cls4 = functional.interpolate(enc4, (5, 5))
+        red_cls4 = self.cls_red4(enc4)
+        al_cls4 = functional.interpolate(red_cls4, (5, 5))
         cls4 = self.cls_branch(al_cls4)
         cls4 = self.cls_out_4(cls4)
 
-        al_cls3 = functional.interpolate(bottleneck, (5, 5))
+        red_cls3 = self.cls_red3(enc3)
+        al_cls3 = functional.interpolate(red_cls3, (5, 5))
         cls3 = self.cls_branch(al_cls3)
         cls3 = self.cls_out_3(cls3)
 
-        al_cls2 = functional.interpolate(bottleneck, (5, 5))
+        red_cls2 = self.cls_red2(enc2)
+        al_cls2 = functional.interpolate(red_cls2, (5, 5))
         cls2 = self.cls_branch(al_cls2)
         cls2 = self.cls_out_2(cls2)
 
-        al_cls1 = functional.interpolate(bottleneck, (5, 5))
+        red_cls1 = self.cls_red1(enc1)
+        al_cls1 = functional.interpolate(red_cls1, (5, 5))
         cls1 = self.cls_branch(al_cls1)
         cls1 = self.cls_out_1(cls1)
 
